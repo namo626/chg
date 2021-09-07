e3639527b28722a30a4f289159be75a930a94b97
Chayanon Wichitrnithed <namo26june@gmail.com>
2021-09-07 17:56:59 -0500

    Update test for netcdf2adcirc

--- test.f90
+++ test.f90
@@ -2,21 +2,27 @@ PROGRAM test
 
   implicit none
 
-  integer ihot, ihotstp, imhsf, iths
+  integer ihot, ihotstp, imhsf, iths, version, node
   real(8) timehsf
   ihot = 68
 
   OPEN(IHOT,FILE='fort.68',ACCESS='DIRECT',RECL=8)
 
   IHOTSTP=1
+  READ(IHOT,REC=IHOTSTP) version
+  ihotstp = 2
+  PRINT *, 'Version = ', version
   READ(IHOT,REC=IHOTSTP) IMHSF
   PRINT *, 'IMHSF is ', IMHSF
-  IHOTSTP=2
+  IHOTSTP=3
   READ(IHOT,REC=IHOTSTP) TIMEHSF
   PRINT *, 'TIME is ', TIMEHSF
-  IHOTSTP=3
+  IHOTSTP=4
   READ(IHOT, rec=ihotstp) iths
   PRINT *, 'ITHS is ', iths
+  IHOTSTP = 5
+  read(ihot, rec=ihotstp) node
+  print *, 'node = ', node
 
 
 
