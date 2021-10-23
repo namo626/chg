16d15cf0e7cdeff37a317b7b7cacf70bc7a28990
namo <namo26june@gmail.com>
2021-10-22 21:37:33 -0500

    Minor fix for getBox.py

--- getBox.py
+++ getBox.py
@@ -4,9 +4,10 @@ import numpy as np
 
 # Number of nodes
 N = 2249093
+filename = fort.14
 
 print("Loading fort.14 ...")
-arr = np.loadtxt('fort.14', skiprows=2, max_rows=N)
+arr = np.loadtxt(filename, skiprows=2, max_rows=N)
 print("Minimum of fort.14:")
 print(np.min(arr,0))
 print("Maximum of fort.14:")
